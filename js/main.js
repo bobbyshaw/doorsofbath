b5ffbc7f483ab15b9c2afaaa65c724896b31dcd4
Tom Robertshaw <me@tomrobertshaw.net>
2014-04-03 19:42:50 +0100

    Some more height tweaks

--- js/main.js
+++ js/main.js
@@ -11,16 +11,26 @@ function DoorsOfBath(header_selector, info_selector, doors_selector) {
 };
 
 DoorsOfBath.prototype.init = function() {
-	this.title.style.left = window.innerWidth * 1/5  + "px";
+	// Fix height of page to height of the window
+	document.getElementsByTagName('body')[0].style.height = window.innerHeight + "px";
 	
+	// Set width of doors UL to account for the number of images.  This gives us the scrolling.
 	var door_width = document.querySelectorAll('.doors img').length * document.querySelector('.doors img').clientWidth;
 	if (door_width > 0) {
 		this.doors.style.width = door_width + "px";
-	}	
+	}
+
+	// Set image height to fill screen
+	var door_imgs = this.doors.querySelectorAll('img');
+	for (var i = 0; i < door_imgs.length; ++i) {
+    	img = door_imgs[i]
+    	img.style.height = window.innerHeight + "px";
+    }
 };
 
 DoorsOfBath.prototype.scroll = function() {
 
+	// Hide the info text once you start scrolling
 	if (window.scrollX > window.innerWidth / 3) {
 		this.info.classList.add('hide');
 	} else {
