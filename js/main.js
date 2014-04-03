c31ecd92f2d4084d6e1923f07c79370c8a3d0ec7
Tom Robertshaw <me@tomrobertshaw.net>
2014-04-03 19:01:59 +0100

    Change the way widths of the door ul works.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
-function DoorsOfBath(header_selector, info_selector) {
+function DoorsOfBath(header_selector, info_selector, doors_selector) {
 	this.title = document.querySelector(header_selector);
 	this.info = this.title.querySelector(info_selector);
+	this.doors = document.querySelector(doors_selector);
 
 	this.init();
 
 	window.addEventListener('resize', this.init.bind(this), false);
 	window.addEventListener('scroll', this.scroll.bind(this), false);
-
-}
+	window.addEventListener('load', this.init.bind(this), false);
+};
 
 DoorsOfBath.prototype.init = function() {
 	this.title.style.left = window.innerWidth * 1/5  + "px";
+	
+	var door_width = document.querySelectorAll('.doors img').length * document.querySelector('.doors img').clientWidth;
+	if (door_width > 0) {
+		this.doors.style.width = door_width + "px";
+	}	
 };
 
 DoorsOfBath.prototype.scroll = function() {
@@ -22,4 +28,4 @@ DoorsOfBath.prototype.scroll = function() {
 	}
 };
 
-var doorsOfBath = new DoorsOfBath('.title', '.info');
+var doorsOfBath = new DoorsOfBath('.title', '.info', '.doors');
